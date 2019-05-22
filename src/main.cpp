23934554050da62bf21ee818e0f4c6d14770b3c9
Maïssa Nouhaud <maissa2.nouhaud@epitech.eu>
2019-05-22 15:43:42 +0200

    [+]CMakeLists.txt : compiling a simple hello world on Linux now going to try it on Windows

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** File Name : main.cpp
+** File description:
+** Maïssa Nouhaud
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "hey you" << std::endl;
+	return 0;
+}
