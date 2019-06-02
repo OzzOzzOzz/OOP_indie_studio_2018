47dc28547ea461219d7f3c2bdd11765651f3c3ec
Mathieu <mathieu.gery@epitech.eu>
2019-06-02 21:37:49 +0200

    [+] textures and mesh

--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -12,10 +12,11 @@ Player::Player(irr::IrrlichtDevice *window, MyEventReceiver *receiver, int x, in
     _receiver = receiver;
     _window = window;
     _mesh = _window->getSceneManager()->getMesh("assets/game/ziggs.md3");
-    _player1 = _window->getSceneManager()->addAnimatedMeshSceneNode( _mesh );
+    _player1 = _window->getSceneManager()->addAnimatedMeshSceneNode(_mesh);
     _player1->setMaterialFlag(irr::video::EMF_LIGHTING, false);
     _player1->setMD2Animation(irr::scene::EMAT_STAND);
     _player1->setMaterialTexture( 0, _window->getVideoDriver()->getTexture("assets/game/ziggs.png") );
+    _player1->setRotation(irr::core::vector3df(50.0f, 50.0f, 50.0f));
 }
 
 Player::~Player()
