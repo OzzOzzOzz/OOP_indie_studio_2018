621e71b79e27218df56723d98eab67960ed5bb89
Maïssa Nouhaud <maissa2.nouhaud@epitech.eu>
2019-06-03 19:28:49 +0200

    [*]Menu.cpp : buttons are in the right place in the main menu

--- src/Menu/Menu.cpp
+++ src/Menu/Menu.cpp
@@ -12,9 +12,9 @@ Menu::Menu(irr::IrrlichtDevice *window)
     _window = window;
     _video = _window->getVideoDriver();
     _sceneManager = _window->getSceneManager();
-	_background = _video->getTexture("assets/menu/main_menu.png");
-	_video->makeColorKeyTexture(_background, irr::core::position2d<irr::s32>(0, 0));
-	initializeButtons();
+    _background = _video->getTexture("assets/menu/main_menu.png");
+    _video->makeColorKeyTexture(_background, irr::core::position2d<irr::s32>(0, 0));
+    initializeButtons();
 }
 
 Menu::~Menu()
@@ -23,33 +23,32 @@ Menu::~Menu()
 
 void Menu::initializeButtons()
 {
-	int size = 450;
-	std::vector<const wchar_t *> buttonsText = {L"Play", L"Load Game", L"Settings", L"Exit"};
+	int size = 350;
+	std::vector<const wchar_t *> buttonsText = {L"", L"", L"", L""};
 
 	for (int i = 0; i < 4; i++) {
-		_mainButtons.push_back(_window->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(1500, size, 1500 + 400, size + 100), nullptr, 0, buttonsText[i]));
-		size += 150;
+		_mainButtons.push_back(_window->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(330, size, 330 + 500, size + 120), nullptr, 0, buttonsText[i]));
+		size += 170;
 	}
 }
 
 int Menu::menuHandling()
 {
 	int ret = 0;
-    _video->beginScene(true, true, irr::video::SColor(255,100,101,140));
+
+	_video->beginScene(true, true, irr::video::SColor(255,100,101,140));
+	_window->getGUIEnvironment()->drawAll();
 	_video->draw2DImage(_background, irr::core::position2d<irr::s32>(0, 0));
 	_sceneManager->drawAll();
-	_window->getGUIEnvironment()->drawAll();
 	ret = buttonHandling();
 	_video->endScene();
-    return (ret);
+    	return (ret);
 }
 
 int Menu::buttonHandling()
 {
-	if (_mainButtons[0]->isPressed()) {
-		std::cout << "Play button pressed" << std::endl;
+	if (_mainButtons[0]->isPressed())
 		return (1);
-	}
 	if (_mainButtons[1]->isPressed())
 		std::cout << "Load Game pressed" << std::endl;
 	if (_mainButtons[2]->isPressed())
