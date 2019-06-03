50a68622f24a31d35002c58124352a144eeb6697
Mathieu <mathieu.gery@epitech.eu>
2019-06-03 15:35:11 +0200

    [+] setroatation to player

--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -15,8 +15,7 @@ Player::Player(irr::IrrlichtDevice *window, MyEventReceiver *receiver, int x, in
     _player1 = _window->getSceneManager()->addAnimatedMeshSceneNode(_mesh);
     _player1->setMaterialFlag(irr::video::EMF_LIGHTING, false);
     _player1->setMD2Animation(irr::scene::EMAT_STAND);
-    _player1->setMaterialTexture( 0, _window->getVideoDriver()->getTexture("assets/game/ziggs.png") );
-    _player1->setRotation(irr::core::vector3df(50.0f, 50.0f, 50.0f));
+    _player1->setMaterialTexture(0, _window->getVideoDriver()->getTexture("assets/game/ziggs.png") );
 }
 
 Player::~Player()
@@ -26,14 +25,22 @@ Player::~Player()
 void Player::Move()
 {
     irr::core::vector3df nodePosition = _player1->getPosition();
-    if(_receiver->IsKeyDown(irr::KEY_KEY_Z))
+    if(_receiver->IsKeyDown(irr::KEY_KEY_Z)) {
+        _player1->setRotation(irr::core::vector3df(0.0f, 180.0f, 0.0f));
         nodePosition.Z += 2.0f;
-    else if(_receiver->IsKeyDown(irr::KEY_KEY_S))
+    }
+    else if(_receiver->IsKeyDown(irr::KEY_KEY_S)) {
         nodePosition.Z -= 2.0f;
-    if(_receiver->IsKeyDown(irr::KEY_KEY_Q))
+        _player1->setRotation(irr::core::vector3df(0.0f, 0.0f, 0.0f));
+    }
+    if(_receiver->IsKeyDown(irr::KEY_KEY_Q)) {
         nodePosition.X -= 2.0f;
-    else if(_receiver->IsKeyDown(irr::KEY_KEY_D))
+        _player1->setRotation(irr::core::vector3df(0.0f, 90.0f, 0.0f));
+    }
+    else if(_receiver->IsKeyDown(irr::KEY_KEY_D)) {
         nodePosition.X += 2.0f;
+        _player1->setRotation(irr::core::vector3df(0.0f, 270.0f, 0.0f));
+    }
     if(_receiver->IsKeyDown(irr::KEY_ESCAPE))
         exit (0);
     _player1->setPosition(nodePosition);
