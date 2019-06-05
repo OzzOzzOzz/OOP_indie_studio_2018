91a65a34de35157f6cc61d12168b7890df5d355b
Remi <remi.bisson@epitech.eu>
2019-06-05 17:38:58 +0200

    [*] Menu.hpp: removed trailing spaces

--- src/Menu/Menu.hpp
+++ src/Menu/Menu.hpp
@@ -25,7 +25,7 @@ public:
     void initializeButtons();
 private:
 	irr::IrrlichtDevice *_window;
-    	irr::video::IVideoDriver *_video;
+	irr::video::IVideoDriver *_video;
 	irr::scene::ISceneManager *_sceneManager;
 	irr::video::ITexture *_background;
 	std::vector<irr::gui::IGUIButton *> _mainButtons;
